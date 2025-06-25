b3d4128316c6a8d80ce0748f21699c8739f1ee79
Shreyansh <149056401+justtshreyansh@users.noreply.github.com>
2025-06-25 10:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const text= document.querySelector("#text");
+const delay = document.querySelector("#delay");
+const button = document.querySelector("#btn");
+const output = document.querySelector("#output");
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function retrieve(){
+	let textVal = text.value.trim();
+	let delayTime = parseInt(delay.value,10);
+	await wait(delayTime);
+	output.innerHTML = `${textVal}`;
+	
+}
+button.addEventListener('click',retrieve);
